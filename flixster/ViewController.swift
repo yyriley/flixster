ead5a53ad946b1af8dc20c34442aa0932c0d940b
Yrua <yyr7@txstate.edu>
2020-09-19 16:42:15 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Yrua Riley on 9/19/20.
+//  Copyright © 2020 yrua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
